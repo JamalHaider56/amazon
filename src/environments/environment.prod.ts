5e52bfea4e7b07ca3660b9fc7bdc15cdf8c7c355
Jamal Haider <jamalhaider@aquila360.com>
2021-08-06 19:01:29 +0500

    fourth commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+
+    apiKey: "AIzaSyDtqi6BIh-FGkdOr2Nq3b6c8hnV3IIVPXo",
+    authDomain: "fir-86f8e.firebaseapp.com",
+    projectId: "fir-86f8e",
+    storageBucket: "fir-86f8e.appspot.com",
+    messagingSenderId: "229278803458",
+    appId: "1:229278803458:web:331421cecd2afce19ae91b",
+    measurementId: "G-SWT8PJ8JSR"
+  }
 };
